2749624c552be3357ef140c0f1a7df05561067cc
opestro <mahdiharzallah21@gmail.com>
2024-04-09 02:45:22 +0100

    middleware active

--- middleware/store.ts
+++ middleware/store.ts
@@ -4,7 +4,7 @@ import { useFetchStore } from '~/stores/store'
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const tenant = await useTenant()
   const fetchStore = useFetchStore()
-  await fetchStore.getStoreData('asynx')
+  await fetchStore.getStoreData(tenant)
   console.log(fetchStore.storeData)
   // Check if user is already logged in (client-side)
   if (fetchStore.storeData) {
